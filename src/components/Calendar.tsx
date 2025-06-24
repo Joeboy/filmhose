65ff1fe7a9db7dad2d0135563b7d31ad3273b42c
Joe Button <j.button@reckondigital.com>
2025-06-24 14:03:01 +0100

    Stylesheet for the calendar widget

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import CalendarWidget from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
+import './Calendar.css';
 import { toNaiveDateString } from '../toNaiveDateString';
 
 interface CalendarProps {
