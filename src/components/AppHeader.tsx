6f4edfbd12816df0db0a199f5057edb2a9c32608
Joe Button <j.button@reckondigital.com>
2025-07-05 14:57:44 +0100

    Add masthead logo

--- src/components/AppHeader.tsx
+++ src/components/AppHeader.tsx
@@ -6,6 +6,11 @@ const AppHeader: React.FC = () => (
   <div id="header-container">
     <div id="header">
       <div className="container" style={{ paddingBottom: '0.5rem' }}>
+        <img
+          src="/logo.svg"
+          alt="FilmHose masthead"
+          style={{ margin: '0.25rem 1rem 0.25rem 0rem', float: 'left', height: '3.5rem' }}
+        />
         <h1>
           <Link to="/">FilmHose</Link>
         </h1>
@@ -14,8 +19,8 @@ const AppHeader: React.FC = () => (
     </div>
     <div className="container">
       <nav>
-        <Link to="/">Home</Link> | <Link to="/cinemas">Cinemas</Link> | <Link to="/about">About</Link> |{' '}
-        <Link to="/help">Help wanted</Link>
+        <Link to="/">Home</Link> | <Link to="/cinemas">Cinemas</Link> |{' '}
+        <Link to="/about">About</Link> | <Link to="/help">Help wanted</Link>
       </nav>
     </div>
   </div>
