f73df00dd60bd138b4d2507bef4df53be602937f
Joe Button <j.button@reckondigital.com>
2026-01-24 15:23:26 +0000

    Update About page text

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -32,11 +32,43 @@ const About: React.FC = () => {
         *definitely* let me know. I'm open to sponsorship / advertising if it's
         appropriate and doesn't compromise the website.
       </p>
-      <p>
-        FilmHose is a <a href="https://react.dev/">React</a> app that lets you
-        browse the data. The source is available{' '}
-        <a href="https://github.com/Joeboy/filmhose">here</a>.
-      </p>
+      <p>Trivia you probably don't care about:</p>
+      <ul>
+        <li>
+          I usually try to run the scrapers (ie. update the listings data) on
+          Saturdays. But also, it only happens when I remember and have time.
+        </li>
+        <li>
+          On average it takes maybe an hour to finish, including a bit of manual
+          intervention. Almost all of that is Romford Lumiere, which is
+          annoyingly resistant to efficient scraping.
+        </li>
+        <li>
+          The "distilled" listings page works by omitting films that have more
+          than ten upcoming screenings in the db. I figure that's a decent way
+          to cut out the noise of all the blockbuster stuff.
+        </li>
+        <li>
+          The site is coded with <a href="https://react.dev/">React</a>. I'm not
+          a proper react developer and a lot of the code is LLM assisted.
+        </li>
+        <li>
+          VSCode just tried to enter an item here about filmhose being a pun on
+          "filmhouse", which it was never intended to be.
+        </li>
+        <li>
+          it's actually a bit annoying that googling filmhose gives search
+          results for filmhouse.
+        </li>
+        <li>
+          Source code is available{' '}
+          <a href="https://github.com/Joeboy/filmhose">here</a>.
+        </li>
+        <li>
+          My total income from the site to date: £5.00. Thanks for the donation,
+          John!
+        </li>
+      </ul>
       <p>Follow the socials on:</p>
       <ul>
         <li>
